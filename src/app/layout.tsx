d935cee2d22dfe95195317112bc77cb34405d690
Carlos T <carlosturpo362@gmail.com>
2025-06-30 21:03:45 +0000

    optimizaremos  y mejoraremos la velocidad de acceso a entre componentes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-inter', // Optional: if you want to use it as a CSS variable
+  variable: '--font-inter',
 });
 
 export const metadata: Metadata = {
