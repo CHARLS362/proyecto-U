299bf15c4e079078ff190c585d5470eb0aebdad7
Carlos T <carlosturpo362@gmail.com>
2025-07-07 15:05:50 +0000

    no le debias de quitar el diseño ni lo colores que tenia originalmente

--- src/app/admin/news/page.tsx
+++ src/app/admin/news/page.tsx
@@ -430,6 +430,12 @@ export default function NewsPage() {
                                 </p>
                             </div>
                         </div>
+                        <div className="h-2.5 flex">
+                            <div className="w-1/4 bg-yellow-400"></div>
+                            <div className="w-1/4 bg-red-500"></div>
+                            <div className="w-1/4 bg-blue-500"></div>
+                            <div className="w-1/4 bg-green-500"></div>
+                        </div>
                       </DialogContent>
                     </Dialog>
                   ))}
